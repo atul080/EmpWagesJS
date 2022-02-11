b0911b286d7aa6c9f19d7ef4dfc71c82c0d93813
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-11 07:38:09 +0530

    Added: gtting max working hrs and days in a month functionality added..

--- EmpWagesBuilder.js
+++ EmpWagesBuilder.js
@@ -3,29 +3,21 @@
  * @Date 10-Feb-2022
  * Employee Wages
  */
-// UC 6 Arrays and 7 Maps
-//constant declarations                        
-const MAX_HRS_IN_MONTH = 160;
-const NUM_OF_WORKING_DAYS = 20;                       
-let totalEmpHrs = 0;                                   
-let totalWorkingDays= 0; 
-let empDailyWageArr = new Array();
-let empDailyWageMap = new Map();
+//constant declarations
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2; 
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20; 
-
-function calcDailyWage(empHrs) 
-     { 
-       return empHrs * WAGE_PER_HOUR;
-     }
-     
-     function getWorkingHours(empCheck) 
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+/**
+ * get Working Hours
+ */
+function getWorkingHours(empCheck) 
+{ 
+    switch (empCheck) //case
     { 
-      switch (empCheck) //case
-      { 
         case IS_PART_TIME: 
             return PART_TIME_HOURS; 
         case IS_FULL_TIME: 
@@ -33,28 +25,15 @@ function calcDailyWage(empHrs)
         default: 
             return 0; 
     } 
-  }
-
-
-function calcDailyWage(empHrs) { 
-    return empHrs * WAGE_PER_HOUR; 
 }
 
-while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
-    totalWorkingDays < NUM_OF_WORKING_DAYS) 
-    {
-        totalWorkingDays++;
-        let empCheck = Math.floor(Math.random() * 10) %3;
-        let empHrs = getWorkingHours(empCheck);
-        totalEmpHrs += empHrs;
-        empDailyWageArr.push(calcDailyWage(empHrs)); 
-        empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+let totalEmpHrs = 0; 
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) //Condition 
+{ 
+    totalWorkingDays++; 
+    let empCheck = Math.floor(Math.random() * 10) % 3; //generate Random
+    totalEmpHrs += getWorkingHours(empCheck); //add hrs
 }
-
-console.log(empDailyWageMap);
-function totalWages(totalWage, dailyWage) { 
-    return totalWage + dailyWage;
-}
-console.log("UC7A — Emp Wage Map totalHrs: " + 
-           Array.from(empDailyWageMap.values()).reduce(totalWages,0));
-  
+let empWage = totalEmpHrs * WAGE_PER_HOUR; //calculate wage
+console.log("Total Days: " + totalWorkingDays + " Total Hrs: " +totalEmpHrs + " Emp Wage: " + empWage); //log output
