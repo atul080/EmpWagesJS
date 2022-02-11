27a9ce530342b52f54617a78befe94131a85cc58
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-11 07:46:32 +0530

    Added: checking employee's presence functionality added..

--- EmpWagesBuilder.js
+++ EmpWagesBuilder.js
@@ -3,24 +3,14 @@
  * @Date 10-Feb-2022
  * Employee Wages
  */
-//constant declarations
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2; 
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20; 
-let empHrs = 0; 
-empCheck = Math.floor(Math.random() * 10) % 3; //generate random 
-switch (empCheck) //case
+const IS_ABSENT = 0 //declare constant
+let empCheck = Math.floor(Math.random() * 10) % 2; //generate random value
+if (empCheck == IS_ABSENT) //condition
 { 
-    case IS_PART_TIME: 
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME: 
-        empHrs = FULL_TIME_HOURS;
-        break; 
-    default: 
-        empHrs = 0; 
+    console.log("Employee is Absent");  //Absent Message
+    return;
 } 
-let empWage = empHrs * WAGE_PER_HOUR; //calculate wage
-console.log("Emp Wage: " + empWage);  //log wage
+else 
+{ 
+    console. log("Employee is Present"); //Present Message
+} 
