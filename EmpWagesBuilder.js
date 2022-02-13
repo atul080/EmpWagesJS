bef45dec4dd16280f691ed00d733222328664b46
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-13 14:11:34 +0530

    Added: Validating employee details using regex functionality added..

--- EmpWagesBuilder.js
+++ EmpWagesBuilder.js
@@ -4,12 +4,7 @@
  * Employee Wages
  */
  class EmployeePayrollData{
-    // property
-    id;
-    salary;                   
-    gender; 
-    startDate;
-   
+                      
     // constructor
     constructor(...params) { 
         this.id = params[0];
@@ -20,19 +15,50 @@
         }
           
     // getter and setter method
+    valueValidation(id,sal) {
+        let valRegex=RegExp('^[1-9][0-9]*$');
+      //  console.log(valRegex.test(id));
+        if((valRegex.test(id))&&(valRegex.test(sal)))
+            {
+                return true;
+            }
+        else throw 'ID/Salary is incorrect!!'
+    }
+    
+    get startDate()
+    {
+        return this._startDate;
+    }
+    set startDate(startDt){
+            let currentDt=new Date();
+            if(startDt<currentDt)
+                this._startDate=startDt;
+            else throw "Invalid start date!!"    
+    }
+    get gender() 
+    { 
+        return this._gender; 
+    }
+    set gender(gender) 
+    { 
+        let genderRegex=RegExp('^[M]|^[F]')
+        if(genderRegex.test(gender))
+            this._gender = gender;
+        //else throw "Gender is incorrect!!";    
+    }
+
     get name() 
     { 
         return this._name; 
     }
-    
     set name(name) 
     { 
-        let nameRegex=RegExp('^[A-Z]{1}{a-z}{3,}$')
+        let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$')
         if(nameRegex.test(name))
             this._name = name;
-        else
-            throw 'Name is Incorrect.'
+        else throw 'Name is incorrect!!'    
     }
+   
     
     // method
     toString() {
@@ -45,16 +71,37 @@
     }
 }
     
-let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000); 
+let employeePayrollData = new EmployeePayrollData(1, "Marks", 30000); 
     console.log(employeePayrollData.toString());
+
     try{
-            employeePayrollData.name = "john";
+            employeePayrollData.name = "Johnn";
+            console.log(employeePayrollData.toString());
+
+            employeePayrollData.gender = "M";
+            console.log(employeePayrollData.toString());
+
+            employeePayrollData.startDate= (new Date-1);
+            console.log(employeePayrollData.toString());
+            
+            if(employeePayrollData.valueValidation(30,68123))
+            {
+                employeePayrollData.id = 30;
+                employeePayrollData.salary = 68000;
+            }    
+            console.log(employeePayrollData.toString());
+            
+            if(employeePayrollData.valueValidation(-3,-68000))
+                {
+                    employeePayrollData.id = -3;
+                    employeePayrollData.salary = -68000;
+                }    
             console.log(employeePayrollData.toString());
     }
     catch(e)
     {
-        console.error(e);
+        console.error(e)
     }
-    let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date()); 
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date()); 
     console.log(newEmployeePayrollData.toString());
    
